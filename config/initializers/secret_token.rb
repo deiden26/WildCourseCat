3d8e790cb79bbacff6a5e86e5a866cf03755f37c
Danny Eiden <deiden26@gmail.com>
2014-02-20 13:40:15 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WildCourseCat::Application.config.secret_key_base = '581c65d17ba68925520f7024e67dad910d4ea091b3b2c6450214cfad815869d13f24925334fcf3ba806ef00de90b4e81a9c0109b5c0d2a48955e021b640b207b'
