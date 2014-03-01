77ba86a4e9b3f687d6e83ea461ab514cd6a5a6eb
Danny Eiden <deiden26@gmail.com>
2014-03-01 15:12:37 -0600

    Improve search area responsiveness

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,31 @@
-function showHide()
+function typeShowHide()
 {
 	var box = $('.courseSearch').val();
-	if ("psych".indexOf(box) != -1)
-		$('.autoCompleteBox').show();
+	if ("psych 303-0".indexOf(box) != -1 || "Psych 303-0".indexOf(box) != -1 || "PSYCH 303-0".indexOf(box) != -1)
+		if(box != "")
+		{
+			$('#results').show();
+			$('#error').hide();
+		}
+		else
+		{
+			$('#results').hide();
+			$('#error').hide();
+		}
 	else
-		$('.autoCompleteBox').hide();
+	{
+		$('#results').hide();
+		$('#error').show();
+	}
+}
+
+function clickHide()
+{
+	$('.autoCompleteBox').hide();
+}
+
+function reveal()
+{
+	$('#greeting').hide();
+	$('.courseSearch').val("Psych 303-0: Psychopathology");
 }
